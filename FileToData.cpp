ca068d549ae684d229786660eb20e7238314cea2
Taylan Smith <possiblyremember@gmail.com>
2024-07-10 06:01:17 -0400

    Decided I'd make this into a seperate project from the code I already made. Need to get better about commenting and actually using headers correctly.

---
+++ FileToData.cpp
@@ -0,0 +1,13 @@
+#include "image_utils.h"
+
+int main(int argc, char **argv) {
+	if (argc == 0) {
+		cerr << "No arguments. Should be path, width, and output.";
+		return -1;
+	}
+	const char* input = argv[1];
+	int width = atoi(argv[2]);
+	const char* output = argv[3];
+	writeBMP(input, width, output);
+	system(output);
+}
